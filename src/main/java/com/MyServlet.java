33cece2f79f0c60b92537868a7b147d308c70cb0
DzhumashevErkin <djumasheverkin@gmail.com>
2022-01-15 17:36:47 +0600

    First

--- src/main/java/com/MyServlet.java
+++ src/main/java/com/MyServlet.java
@@ -1,92 +1,18 @@
 package com;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.print("<h1> My Servlet </h1>");
+//        PrintWriter out = resp.getWriter();
+//        out.print("<h1> My Servlet </h1>");
+        req.setAttribute("myAttr","Java");
+        req.getRequestDispatcher("index.jsp").forward(req,resp);
     }
 }
-//@WebServlet("/primeNumber")
-//public class PrimeNumber extends HttpServlet {
-//    private boolean checkSimple(int n) {
-//        for (int i = 2; i <= n / 2; i++)
-//            if ((n % i) == 0)
-//                return false;
-//        return true;
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        List<Integer> tempList = new ArrayList<>();
-//        List<PrimeNumberModel> list = new ArrayList<>();
-//        int count = 0, i = 2;
-//        while (count <= 100) {
-//            if (checkSimple(i)) {
-//                count++;
-//                tempList.add(i);
-//            }
-//            i++;
-//        }
-//        count = 0;
-//        for (int j = 0; j < tempList.size(); j += 7) {
-//            if (j >= 98)
-//                break;
-//            if (count == 0) {
-//                count = 1;
-//                list.add(new PrimeNumberModel(
-//                        new Integer[]{
-//                                tempList.get(j),
-//                                tempList.get(j + 1),
-//                                tempList.get(j + 2),
-//                                tempList.get(j + 3),
-//                                tempList.get(j + 4),
-//                                tempList.get(j + 5),
-//                                tempList.get(j + 6),
-//                                tempList.get(j + 7)},
-//                        new String[]{
-//                                "white",
-//                                "black",
-//                                "white",
-//                                "black",
-//                                "white",
-//                                "black",
-//                                "white",
-//                                "black"}));
-//            } else if (count == 1) {
-//                count = 0;
-//                list.add(new PrimeNumberModel(
-//                        new Integer[]{
-//                                tempList.get(j),
-//                                tempList.get(j + 1),
-//                                tempList.get(j + 2),
-//                                tempList.get(j + 3),
-//                                tempList.get(j + 4),
-//                                tempList.get(j + 5),
-//                                tempList.get(j + 6),
-//                                tempList.get(j + 7)},
-//                        new String[]{
-//                                "black",
-//                                "white",
-//                                "black",
-//                                "white",
-//                                "black",
-//                                "white",
-//                                "black",
-//                                "white"}));
-//            }
-//        }
-//        req.setAttribute("list", list);
-//        req.getRequestDispatcher("/pages/ChessBoard.jsp").forward(req, resp);
-//    }
-//}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,6 +6,10 @@
         int b = 200; %>
     <%= a + b%>
 </H1>
+
+<p>
+<h1>${myAttr}</h1>
+</p>
 </body>
 </html>
 
